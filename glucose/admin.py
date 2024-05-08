6092cfbe6eb303d7b8fb4228adf2ac9d9934f72e
mugdhakolte <koltemugdha@gmail.com>
2024-05-08 16:54:29 +0200

    Added admin interface for all models

--- glucose/admin.py
+++ glucose/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
 
-# Register your models here.
+from glucose.models import User, Device, GlucoseLevel
+
+
+class UserAdmin(admin.ModelAdmin):
+    search_fields = [
+        "user_id",
+    ]
+    list_per_page = 50
+
+
+class DeviceAdmin(admin.ModelAdmin):
+    search_fields = [
+        "serial_no",
+        "name",
+        "user__user_id",
+    ]
+    list_per_page = 50
+
+
+class GlucoseLevelAdmin(admin.ModelAdmin):
+    search_fields = [
+        "device__user__user_id",
+        "device__name",
+        "device__serial_no",
+        "device_timestamp",
+        "recording_type",
+    ]
+    list_per_page = 50
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Device, DeviceAdmin)
+admin.site.register(GlucoseLevel, GlucoseLevelAdmin)
